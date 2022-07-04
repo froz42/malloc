a77c0edac2bfc6739f271a2eb106df9cb01b1a55
tmatis <tmatis@student.42.fr>
2022-07-04 13:39:49 +0200

    fix abs

--- src/memory_view/draw_utils.c
+++ src/memory_view/draw_utils.c
@@ -87,6 +87,19 @@ static int sign(int x)
 }
 
 /**
+ * @brief return the absolute value of a number
+ * 
+ * @param x the number
+ * @return int the absolute value
+ */
+static int ft_abs(int x)
+{
+	if (x < 0)
+		return -x;
+	return x;
+}
+
+/**
  * @brief Draw a line using Bresenham algorithm
  * 
  * @param f the frame ptr
@@ -102,8 +115,8 @@ void draw_line(t_frame *f, int x1, int y1, int x2, int y2, int color)
 
 	x = x1;
 	y = y1;
-	dx = abs(x2 - x1);
-	dy = abs(y2 - y1);
+	dx = ft_abs(x2 - x1);
+	dy = ft_abs(y2 - y1);
 	s1 = sign(x2 - x1);
 	s2 = sign(y2 - y1);
 	swap = 0;
